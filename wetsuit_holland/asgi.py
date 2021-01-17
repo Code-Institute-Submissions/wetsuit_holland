2517db24e68fb16f0fd198b1d72153fc6dd99988
remcokes <rkes@stadgenoot.nl>
2021-01-17 14:52:57 +0000

    asgi

--- wetsuit_holland/asgi.py
+++ wetsuit_holland/asgi.py
@@ -2,9 +2,6 @@
 ASGI config for wetsuit_holland project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
